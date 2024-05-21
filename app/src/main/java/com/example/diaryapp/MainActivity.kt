f777aa22749e2ea248c517736a75d5d11ab45daa
mhmdfarhan28 <mhmdfarhan28@gmail.com>
2024-05-21 15:27:23 +0700

    initial commit

---
+++ app/src/main/java/com/example/diaryapp/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.diaryapp
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.compose.rememberNavController
+import com.example.diaryapp.data.database.ImagesToDeleteDao
+import com.example.diaryapp.data.database.ImagesToUploadDao
+import com.example.diaryapp.data.repository.MongoDB
+import com.example.diaryapp.navigation.Screen
+import com.example.diaryapp.navigation.SetupNavGraph
+import com.example.diaryapp.ui.theme.DiaryAppTheme
+import com.example.diaryapp.util.Constants.APP_ID
+import com.example.diaryapp.util.retryDeletingImageFromFirebase
+import com.example.diaryapp.util.retryUploadingImageToFirebase
+import com.google.firebase.FirebaseApp
+import dagger.hilt.android.AndroidEntryPoint
+import io.realm.kotlin.mongodb.App
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@RequiresApi(Build.VERSION_CODES.N)
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var imagesToUploadDao: ImagesToUploadDao
+    @Inject
+    lateinit var imagesToDeleteDao: ImagesToDeleteDao
+    private var keepSplashOpened = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition {
+            keepSplashOpened
+        }
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        FirebaseApp.initializeApp(this)
+        setContent {
+            DiaryAppTheme {
+                val navController = rememberNavController()
+                SetupNavGraph(
+                    startDestination = getStartDestination(),
+                    navController = navController,
+                    onDataLoaded = {
+                        keepSplashOpened = false
+                    }
+                )
+            }
+        }
+
+        /*cleanupCheck(
+            scope = lifecycleScope,
+            imagesToUploadDao = imagesToUploadDao,
+            imagesToDeleteDao = imagesToDeleteDao
+        )*/
+    }
+}
+private fun cleanupCheck(
+    scope: CoroutineScope,
+    imagesToUploadDao: ImagesToUploadDao,
+    imagesToDeleteDao: ImagesToDeleteDao
+) {
+    scope.launch(Dispatchers.IO) {
+        val result = imagesToUploadDao.getAllImages()
+        Log.d("cleanupCheck", "cleanupCheck: $result")
+        result.forEach { imageToUpload ->
+            retryUploadingImageToFirebase(
+                imageToUpload = imageToUpload,
+                onSuccess = {
+                    scope.launch(Dispatchers.IO) {
+                        imagesToUploadDao.cleanupImage(imageId = imageToUpload.id)
+                    }
+                }
+            )
+        }
+        val result2 = imagesToDeleteDao.getAllImages()
+        result2.forEach { imageToDelete ->
+            retryDeletingImageFromFirebase(
+                imageToDelete = imageToDelete,
+                onSuccess = {
+                    scope.launch(Dispatchers.IO) {
+                        imagesToDeleteDao.cleanupImage(imageId = imageToDelete.id)
+                    }
+                }
+            )
+        }
+    }
+}
+
+private fun getStartDestination(): String {
+    val user = App.create(APP_ID).currentUser
+    return if (user != null && user.loggedIn) Screen.Home.route
+    else Screen.Authentication.route
+}
